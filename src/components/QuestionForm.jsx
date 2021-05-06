edee31c74b256f2b88d1fffa7ae382e31868b6bb
Marcio Campos <cmpsmarcio@gmail.com>
2021-05-05 23:05:53 -0300

    Ajustes no formulario. Pegando dados do Form

--- src/components/QuestionForm.jsx
+++ src/components/QuestionForm.jsx
@@ -3,10 +3,7 @@ import Bunner from './Bunner.jsx'
 import { Button, Form, Input, Select, Label, InputNumber, InputDate } from './styled-components/index.js'
 
 export default function QuestionForm () {
-
-  const [firstDose, setFirstDose] = useState(null)
-  const [secondDose, setSecondDose] = useState(null)
-  const [birthDate, setBirthDate] = useState(null)
+  const [form, setForm] = useState({})
 
   function validateMaxLength(event, maxLength) {
     if (event.target.value.length >= maxLength) {
@@ -14,50 +11,62 @@ export default function QuestionForm () {
     }
   }
 
+  function handleChange(event) {
+    setForm({
+      ...form,
+      [event.target.name] : event.target.value
+    })
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log(form)
+  }
+
   return (
-     <Form>
+     <Form onSubmit={e => handleSubmit(e)}>
       <Bunner />
       <Label htmlFor="registration">Matrícula</Label>
-      <InputNumber id="registration" type="text" placeholder="Digite sua matrícula" onBeforeInput={(e) => validateMaxLength(e, 6)}></InputNumber>
+      <InputNumber id="registration" name="register" type="text" placeholder="Digite sua matrícula" onBeforeInput={(e) => validateMaxLength(e, 6)} onChange={e => handleChange(e)} required></InputNumber>
       <Label htmlFor="document">CPF</Label>
-      <InputNumber id="document" placeholder="Digite seu CPF" onBeforeInput={(e) => validateMaxLength(e, 11)}></InputNumber>
+      <InputNumber id="document" name="document" placeholder="Digite seu CPF" onBeforeInput={(e) => validateMaxLength(e, 11)}  onChange={e => handleChange(e)} required></InputNumber>
       <Label htmlFor="name">Nome completo</Label>
-      <Input id="name" type="text" placeholder="Digite seu nome completo" maxLength="130"></Input>
+      <Input id="name" name="name" type="text" placeholder="Digite seu nome completo" maxLength="130"  onChange={e => handleChange(e)} required></Input>
       <Label htmlFor="age">Data de nascimento</Label>
-      <InputDate id="birth" dateFormat="dd/MM/yyyy" selected={birthDate} onChange={date => setBirthDate(date)}></InputDate>
+      <InputDate id="birth" name="birthday" onChange={e => handleChange(e)} required></InputDate>    
       <Label htmlFor="alreadyInfected">Já teve Covid-19?</Label>
-      <Select id="alreadyInfected">
+      <Select id="alreadyInfected" name="alreadyInfected" onChange={e => handleChange(e)}>
         <option value="false">Não</option>
         <option value="true">Sim</option>
       </Select>
       <Label htmlFor="symptomsSeverity">Como foram os sintomas?</Label>
-      <Select id="symptomsSeverity">
-        <option value="none">Não se aplica</option>
-        <option value="light">Leves</option>
-        <option value="severe">Graves</option>
+      <Select id="symptomsSeverity" name="symptomsSeverity" onChange={e => handleChange(e)}>
+        <option value="ASYMPTOMATIC">Não se aplica</option>
+        <option value="LIGHT">Leves</option>
+        <option value="SERIOUS">Graves</option>
       </Select>
       <Label htmlFor="symptoms">Quais foram sintomas:</Label>
-      <Input id="symptoms" placeholder="febre, dor de cabeça..."></Input>
+      <Input id="symptoms" name="symptoms" placeholder="febre, dor de cabeça..." onChange={e => handleChange(e)} ></Input>
       <Label htmlFor="symptomsRemained">Os sintomas permaneceram após a recuperação?</Label>
-      <Select id="symptomsRemained">
+      <Select id="symptomsRemained" name="symptomsRemained" onChange={e => handleChange(e)}>
         <option value="false">Não</option>
         <option value="true">Sim</option>
       </Select>
       <Label htmlFor="contactWithInfected">Teve contato com alguém infectado recentemente?</Label>
-      <Select id="contactWithInfected">
+      <Select id="contactWithInfected" name="contactWithInfectedRecently" onChange={e => handleChange(e)}>
         <option value="false">Não</option>
         <option value="true">Sim</option>
       </Select>
       <Label htmlFor="aleadyVacinated">Já foi vacinado?</Label>
-      <Select id="aleadyVacinated">
+      <Select id="aleadyVacinated" name="alreadyVaccinated" onChange={e => handleChange(e)}>
         <option value="false">Não</option>
         <option value="true">Sim</option>
       </Select>
       <Label htmlFor="vaccinatedDate">Data da 1ª dose</Label>
-      <InputDate id="vaccinatedDate" dateFormat="dd/MM/yyyy" selected={firstDose} onChange={date => setFirstDose(date)}></InputDate>
+      <InputDate id="firstVaccinatedDate" name="firstVaccinatedDate" onChange={e => handleChange(e)} ></InputDate>
       <Label htmlFor="vaccinatedDate">Data da 2ª dose</Label>
-      <InputDate id="vaccinatedDate" dateFormat="dd/MM/yyyy" selected={secondDose} onChange={date => setSecondDose(date)}></InputDate>
-      <Button>Enviar</Button>
+      <InputDate id="secondVaccinatedDate" name="secondVaccinatedDate" onChange={e => handleChange(e)}></InputDate>
+      <Button type="submit">Enviar</Button>
     </Form>
   )
 }
